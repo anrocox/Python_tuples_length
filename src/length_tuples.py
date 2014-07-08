ebaa074a96b71d1e251034756645259883e1c2e1
Andres Rodriguez <anrocox@gmail.com>
2014-07-07 19:27:19 -0500

    In python how to know the size of the tuple?

---
+++ src/length_tuples.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 7, 2014
+
+@author: anroco
+
+In python how to know the size of the tuple?
+
+En python ¿como se determina el tamaño de la tupla?
+'''
+
+#create a tuple
+tupla = tuple('length of a tuple')
+print (tupla)
+
+#know the length of a tuple, use the len() function built-in python
+print (len(tupla))
